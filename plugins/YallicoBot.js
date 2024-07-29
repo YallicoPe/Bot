6d7765c287035c4e05de760eac829b2df6e4db19
YallicoPe <yhairmusica950@gmail.com>
2024-07-29 07:02:25 -0500

    iniciado

---
+++ plugins/YallicoBot.js
@@ -0,0 +1,47 @@
+const handler = async (m, {conn}) => {
+  m.reply(global.Yallico);
+};
+handler.command = /^(Yallico|Yallico)$/i;
+export default handler;
+
+global.Yallico = `
+*Yallico Bot Informacion 🇦🇱*
+
+_Bot Creado Con La Finalidad De Dar Una Experiencia Única Y Mejorar El Estatus De Algún Grupo De WhatsApp 🥶_
+
+*Funciones:*
+_🇦🇱 Descargar Videos Y Música_
+_🇦🇱 Crear Stickers_
+_🇦🇱 Notificar A Todos_
+_🇦🇱 Juegos Ramdons_
+_🇦🇱 Etc_
+
+*Nota:* _Los Precios Pueden Tanto Subir Como Bajar En Los Días Por El Tema Del Cambio De Moneda_
+
+*Aclaraciones:* _Una Vez Usted Haya Adquirido A Yallico Bot , No Existe Devolución Ni Reembolsos_
+
+*Bot Para Grupo 🇦🇱*
+_🇵🇪 5 Soles_
+_🇨🇱 1,550 Clp_
+_🇦🇷 2,150 Ars_
+_🇲🇽 32 Mx_
+_🇧🇴 16 Bs_
+_🇨🇴 6,500 Cop_
+_🇺🇾 70 Uru_
+_🌏 2,60 Usd_
+
+*BOT P. PERSONALIZADO 🇦🇱*
+_🇵🇪 20 Soles_
+_🇨🇱 6,200 Clp_
+_🇦🇷 8,600 Ars_
+_🇲🇽 127 Mx_
+_🇧🇴 64 Bs_
+_🇨🇴 26,000 Cop_
+_🇺🇾 280 Uru_
+_🌏 12 Usd_
+
+*Nota :*_Los Usd Están Con Comisión Incluida_
+
+*Grupo De Ventas Y Pruebas*
+${nna}
+`;
